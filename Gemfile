13e99ed8fe462f2166bbcf5495e3bfa12f2b0537
cokeefe <chaemobile@gmail.com>
2013-11-11 20:49:13 -0500

    mongoid rspec cucumber setup

---
+++ Gemfile
@@ -0,0 +1,21 @@
+source 'https://rubygems.org'
+
+gem 'rails', '4.0.0'
+gem 'sass-rails', '~> 4.0.0'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.0.0'
+gem 'jquery-rails'
+gem 'turbolinks'
+gem 'jbuilder', '~> 1.2'
+gem 'mongoid', git: 'git://github.com/mongoid/mongoid.git'
+gem 'bson_ext'
+
+group :test do
+  gem 'rspec-rails'
+  gem 'cucumber-rails', :require => false
+  gem 'database_cleaner'
+end
+
+group :doc do
+  gem 'sdoc', require: false
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WolfT::Application.config.secret_key_base = 'c7c18ffa536f1d0907ccab4a918fe454fa5942384631f29a9e724299de5a06997f9f4eaa6ab5ea6017cb7522e20b4742f75b132c53840e23e8e9c3fd8d172fa3'
