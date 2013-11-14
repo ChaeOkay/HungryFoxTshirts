6284e351ee2c7d7945a0e09e40323f297a9ae918
cokeefe <chaemobile@gmail.com>
2013-11-14 17:46:10 -0500

    create seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+tshirt = Tshirt.create(
+                name: 'Duran Duran - 00A'
+                description: 'muscle shirt, vintage 80s, cotton, white.'
+                cost: 20)
+
+tshirt.tsizes << Tsize.create(name: 'm')
