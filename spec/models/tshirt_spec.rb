2c9ee966ba1fea364706cea1ffbccf5da2815214
cokeefe <chaemobile@gmail.com>
2013-11-14 17:38:57 -0500

    update size to tsize name, add assoc test

--- spec/models/tshirt_spec.rb
+++ spec/models/tshirt_spec.rb
@@ -5,5 +5,16 @@ describe Tshirt do
   it { should validate_presence_of :description }
   it { should validate_presence_of :cost }
 
-  it { should have_many :sizes }
+  it { should have_many :tsizes }
+
+  context 'with valid parameters' do
+    let(:tshirt) { Tshirt.create( name: '001',
+                                  description: 'hot',
+                                  cost: 10) }
+
+    it 'should have a size' do
+      tshirt.tsizes << Tsize.create(name: 's')
+      expect(tshirt.tsizes.count).to eq 1
+    end
+  end
 end
