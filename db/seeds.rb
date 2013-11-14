769ff20de09acbf4568cafac7ee6364d753111c4
cokeefe <chaemobile@gmail.com>
2013-11-14 17:48:05 -0500

    typo in tshirt create

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 tshirt = Tshirt.create(
-                name: 'Duran Duran - 00A'
-                description: 'muscle shirt, vintage 80s, cotton, white.'
+                name: 'Duran Duran - 00A',
+                description: 'muscle shirt, vintage 80s, cotton, white.',
                 cost: 20)
 
 tshirt.tsizes << Tsize.create(name: 'm')
