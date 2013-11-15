13bb129d63a98ec5b6093a5ec28adc6e72e687ff
cokeefe <chaemobile@gmail.com>
2013-11-14 20:48:56 -0500

    landing erb to render feature tshirt attributes and sizes

--- app/controllers/statics_controller.rb
+++ app/controllers/statics_controller.rb
@@ -1,5 +1,7 @@
 class StaticsController < ApplicationController
   def landing
+    @tshirt_feature = Tshirt.find_by(name: 'Duran Duran - 00A')
+    @sizes = @tshirt_feature.tsize.pluck(:name)
   end
 
   def about
