1e4981837349f5155e75fe5234b21e24b5a09d15
cokeefe <chaemobile@gmail.com>
2013-11-17 15:50:48 -0500

    update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 tshirt = Tshirt.create(
                 name: 'Duran Duran - 00A',
                 description: 'muscle shirt, vintage 80s, cotton, white.',
-                cost: 20)
+                cost: 80)
 
 tshirt.tsize << Tsize.create(name: 'm')
