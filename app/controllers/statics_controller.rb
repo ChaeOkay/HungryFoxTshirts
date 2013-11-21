768ac0161ead9553608e6852b7c413eae7e0f356
cokeefe <chaemobile@gmail.com>
2013-11-21 17:08:59 -0500

    add ajax to brand / feature link

--- app/controllers/statics_controller.rb
+++ app/controllers/statics_controller.rb
@@ -4,6 +4,11 @@ class StaticsController < ApplicationController
     @tshirt = Tshirt.all.sample
   end
 
+  def feature
+    @tshirt = Tshirt.all.sample
+    render 'statics/feature', layout: false
+  end
+
   def about
     render "statics/about", layout: false
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ WolfT::Application.routes.draw do
   root to: 'statics#landing'
   resources :tshirts, only: :index
 
+  get '/feature', to: 'statics#feature'
   get '/about', to: 'statics#about'
 end
