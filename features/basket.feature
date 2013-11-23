cb263ef689235971647ca0b38178b58f1b4c0a46
cokeefe <chaemobile@gmail.com>
2013-11-22 20:12:43 -0500

    update and add basket feature spec

---
+++ features/basket.feature
@@ -0,0 +1,12 @@
+Feature: Basket
+In order for customers to buy a t-shirt
+As a customer
+I want to have items in a basket
+
+Scenario: Add Item to Basket
+  Given I am at the landing page
+  And I see the inventory
+  And I set the size to 'm'
+  And I set the quantity to '2'
+  When I add the tshirt to the basket
+  Then the Basket should have '2' items
---
+++ features/step_definitions/basket_steps.rb
@@ -0,0 +1,19 @@
+Given(/^I see the inventory$/) do
+  page.should have_content @t.name
+end
+
+When(/^I set the size to 'm'$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I set the quantity to '(\d+)'$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I add the tshirt to the basket$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^the Basket should have '(\d+)' items$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
