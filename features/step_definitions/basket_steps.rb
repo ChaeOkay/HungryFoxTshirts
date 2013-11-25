56eccd34d7dcbba8cb1a59339d577847d5ee3619
cokeefe <chaemobile@gmail.com>
2013-11-24 22:25:31 -0500

    update basket feature spec

--- features/step_definitions/basket_steps.rb
+++ features/step_definitions/basket_steps.rb
@@ -1,19 +1,20 @@
 Given(/^I see the inventory$/) do
+  click_link 'inventory'
   page.should have_content @t.name
 end
 
-When(/^I set the size to 'm'$/) do
-  pending # express the regexp above with the code you wish you had
+When(/^I set the size to "(.*?)"$/) do |option|
+  page.select(option.upcase, :from => 'tshirt_size')
 end
 
-When(/^I set the quantity to '(\d+)'$/) do |arg1|
-  pending # express the regexp above with the code you wish you had
+When(/^I set the quantity to '(\d+)'$/) do |qty|
+  page.select(qty, :from => 'tshirt_qty')
 end
 
 When(/^I add the tshirt to the basket$/) do
-  pending # express the regexp above with the code you wish you had
+  click_link 'Add to basket'
 end
 
 Then(/^the Basket should have '(\d+)' items$/) do |arg1|
-  pending # express the regexp above with the code you wish you had
+  pending
 end
