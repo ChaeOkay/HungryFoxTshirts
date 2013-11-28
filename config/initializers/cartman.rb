c8eed6e95d4120de51aaef6c75a439973d67dabd
cokeefe <chaemobile@gmail.com>
2013-11-28 17:01:57 -0500

    redistogo

--- config/initializers/cartman.rb
+++ config/initializers/cartman.rb
@@ -1,6 +1,8 @@
+uri = URI.parse(ENV["REDISTOGO_URL"])
+
 Cartman.config do |c|
   c.cart_expires_in = 10800 #3 hours
   c.unit_cost_field = :unit_cost # for cart totaling
   c.quantity_field = :quantity # for quantity totaling
-  c.redis = Redis.new(path: REDIS_PROVIDER)
+  c.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 end
