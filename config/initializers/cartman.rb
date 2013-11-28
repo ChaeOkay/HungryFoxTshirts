a8fb83570b717a6ad99185675d8a7cd195448ce5
cokeefe <chaemobile@gmail.com>
2013-11-28 16:54:24 -0500

    redis path

--- config/initializers/cartman.rb
+++ config/initializers/cartman.rb
@@ -2,5 +2,5 @@ Cartman.config do |c|
   c.cart_expires_in = 10800 #3 hours
   c.unit_cost_field = :unit_cost # for cart totaling
   c.quantity_field = :quantity # for quantity totaling
-  c.redis = Redis.new # set the redis connection here
+  c.redis = Redis.new(path: REDIS_PROVIDER)
 end
