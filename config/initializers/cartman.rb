8a72a6129a9ceea440da1056687349c76e7bfbe0
cokeefe <chaemobile@gmail.com>
2013-11-28 17:11:07 -0500

    redis initializer

--- config/initializers/cartman.rb
+++ config/initializers/cartman.rb
@@ -1,8 +1,6 @@
-uri = URI.parse(ENV["REDISTOGO_URL"])
-
 Cartman.config do |c|
   c.cart_expires_in = 10800 #3 hours
   c.unit_cost_field = :unit_cost # for cart totaling
   c.quantity_field = :quantity # for quantity totaling
-  c.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+  c.redis = REDIS # set the redis connection here
 end
