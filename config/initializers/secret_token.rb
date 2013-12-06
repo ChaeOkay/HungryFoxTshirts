5b99c4992b3856542f9c7d7b9d44b8abec2e44dd
cokeefe <chaemobile@gmail.com>
2013-12-06 09:48:49 -0500

    update env variables

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,6 +1,2 @@
 secret = ENV['WOLFT_TOKEN']
-if secret.length < 30
-  raise "Secret token cannot be loaded"
-else
-  WolfT::Application.config.secret_key_base = secret
-end
+WolfT::Application.config.secret_key_base = secret
