163ad1f210703172cf213936bfce0e37c0ef33bd
cokeefe <chaemobile@gmail.com>
2013-12-08 19:05:39 -0500

    add order factory

--- spec/factories.rb
+++ spec/factories.rb
@@ -18,4 +18,28 @@ FactoryGirl.define do
   factory :picture do
     view 'front'
   end
+
+  factory :order do
+    stripeToken '123'
+    stripeEmail 'test@test.com'
+    stripeBillingName 'test'
+    stripeBillingAddressLine1 'test address'
+    stripeBillingAddressZip '12345'
+    stripeBillingAddressCity 'testcity'
+    stripeBillingAddressState 'teststate'
+    stripeBillingAddressCountry 'testcountry'
+
+    stripeShippingName 'testselfshipping'
+    stripeShippingAddressLine1 'test address'
+    stripeShippingAddressZip '12345'
+    stripeShippingAddressCity 'testcity'
+    stripeShippingAddressState 'teststate'
+    stripeShippingAddressCountry 'testcountry'
+
+    basketTotal '200'
+    basketItemQuantity '1'
+    basketDescription 'tshirt'
+
+    record_number '234ACD'
+  end
 end
