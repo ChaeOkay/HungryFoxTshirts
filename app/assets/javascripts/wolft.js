51000ffc4aee37eb0b66732f5c1e0c29970a9306
cokeefe <chaemobile@gmail.com>
2013-12-20 12:01:12 -0500

    js refactor

--- app/assets/javascripts/wolft.js
+++ app/assets/javascripts/wolft.js
@@ -1,48 +1,58 @@
-function showPage(data) {
-  $('#maincontent').fadeOut(function(){
-    $('#maincontent').html(data)
-    $('#maincontent').fadeIn()
+function showPage(selector, data, foundationBind) {
+  $(selector).fadeOut(function(){
+    $(selector).html(data)
+    $(selector).fadeIn()
+
+    if (foundationBind != undefined) {
+      $(foundationBind).foundation('orbit', 'reflow')
+    }
   })
 }
 
-function Navbar() {
+function Navbar(id) {
   this.content = $('#maincontent')
+  this.id = id
 }
 
 Navbar.prototype = {
-  call: function(id){
+  call: function(foundationBind){
+    var that = this.content
+    var id = this.id
     $(id).on('click', function(e){
       e.preventDefault()
-      $(id).bind('ajax:success', function(e, data, xhr, settings){
-        showPage(data)
+      $(id).bind('ajax:success', function(e, data){
+        showPage(that, data, foundationBind)
       })
     })
-  },
+  }
+}
 
+function Inventory(){
+}
+
+Inventory.prototype = {
   submit: function(){
-    $('#maincontent').on('submit', '#tform', function(e){
+    var content = $('#maincontent')
+    $(content).on('submit', '#tform', function(e){
       e.preventDefault()
+      console.log('in the submit function!')
       var $data = $(this).closest('form').serialize()
       $.ajax({
         type: 'post',
         url: '/add',
         data: $data
       }).done(function(data){
-        showPage(data)
+        showPage(content, data)
       }).fail(function(jqXHR, textStatus, errorThrown){
         console.log(errorThrown)
       })
     })
-  }
-}
+  },
 
-function TForm() {
-}
-
-TForm.prototype = {
   updateQty: function(){
     $('#maincontent').on('change', '#tshirt_size', function(e){
       e.preventDefault()
+      console.log('in the updateQty function!')
       var shirt = this
       var $data = $(this).closest('form').serialize()
 
@@ -51,7 +61,7 @@ TForm.prototype = {
         url: '/show_stock',
         data: $data
       }).done(function(data){
-        qty = $(shirt).parent().next().children('#tshirt_qty')
+        qty = $(shirt).closest('.form_fields').find('#tshirt_qty')
         qty.html('option')
 
         $.each(data, function(){
@@ -69,26 +79,35 @@ function Basket(){
 
 Basket.prototype = {
   empty: function(){
-    $('#maincontent').on('click', '#ClearBasket', function(e){
+    var content = $('#maincontent')
+    $(content).on('click', '#ClearBasket', function(e){
       e.preventDefault()
       $.get( "/empty", function( data ) {
-        showPage(data)
+        showPage(content, data)
       });
     })
   }
 }
 
-basket = new Basket()
-tForm = new TForm()
-nav = new Navbar()
+var initialize = function(){
+  inventoryLink = new Navbar('#inventory')
+  inventoryLink.call()
 
-$(document).ready(function(){
-  nav.call('#about')
-  nav.call('#inventory')
-  nav.call('#feature')
-  nav.call('#basket')
-  nav.submit()
+  aboutLink = new Navbar('#about')
+  aboutLink.call()
 
-  tForm.updateQty()
+  basketLink = new Navbar('#basket')
+  basketLink.call()
+
+  featureLink = new Navbar('#feature')
+  featureLink.call('#feature-tshirt')
+
+  inventory = new Inventory()
+  inventory.submit()
+  inventory.updateQty()
+
+  basket = new Basket()
   basket.empty()
-})
+}
+
+$(document).ready(initialize)
